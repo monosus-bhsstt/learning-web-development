4547a275b7d414245c304ef1ec2b3db8ceb616f8
mono-yano <yano@monosus.co.jp>
2022-04-07 17:25:56 +0900

    rm:script.js(不要なconsole.logの削除)

--- 02_progress-steps/script.js
+++ 02_progress-steps/script.js
@@ -3,14 +3,10 @@ const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const circles = document.querySelectorAll('.circle')
 
-console.log(circles);
-
 let currentActive = 1
-console.log(`prev1:${prev.disabled}`);
+
 next.addEventListener('click', () => {
   currentActive++
-  console.log(`prev2:${prev.disabled}`);
-  console.log(`first:${currentActive}`);
 
   if(currentActive > circles.length) {
       currentActive = circles.length
@@ -18,13 +14,9 @@ next.addEventListener('click', () => {
 
 
   circles.forEach((circle, idx) => {
-console.log(`third:${idx}`);
-console.log(`second:${currentActive}`);
     if(idx < currentActive) {
-console.log(`true`);
       circle.classList.add('active')
     } else {
-console.log(`falce`);
       circle.classList.remove('active')
     }
 })
@@ -32,18 +24,11 @@ console.log(`falce`);
 const actives = document.querySelectorAll('.active')
 
 progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
-console.log(progress);
-console.log(`width:${progress.style.width}`);
-console.log(`actives:${actives.length - 1}`);
-console.log(`circles:${circles.length - 1}`);
 
 if(currentActive === 1) {
     prev.disabled = true
-    console.log(`prev3:${prev.disabled}`);
-    console.log(`prev2:${prev.disabled}`);
 } else if(currentActive === circles.length) {
     next.disabled = true
-    console.log(`next:${next.disabled}`);
 } else {
     prev.disabled = false
     next.disabled = false
