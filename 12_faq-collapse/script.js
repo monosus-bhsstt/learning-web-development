3b137d6863583b96612fc2c9857a535b22f86d1f
mono-yano <yano@monosus.co.jp>
2022-04-19 15:21:29 +0900

    add:script.js(読み取り)

---
+++ 12_faq-collapse/script.js
@@ -0,0 +1,8 @@
+const toggles = document.querySelectorAll('.faq-toggle')
+
+//"faq-toggle"のクラス名のあるタグをクリックした時。その親要素にクラス名"active"があれば削除して、なければ追加する。
+toggles.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    toggle.parentNode.classList.toggle('active')
+  })
+})
