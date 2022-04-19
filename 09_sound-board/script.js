e8c11b34ae2ce603d7d5cdbc6de4d6ce3f58e7be
mono-yano <yano@monosus.co.jp>
2022-04-19 18:19:42 +0900

    add:script.js(読み取り2)

--- 09_sound-board/script.js
+++ 09_sound-board/script.js
@@ -5,27 +5,24 @@ sounds.forEach(sound => {
   const btn = document.createElement('button')
   //上記の"btn"にclass名'btn'を付与
   btn.classList.add('btn')
-
   //"btn"に配列として宣言した"sounds"の各単語をテキストとして代入
   btn.innerText = sound
   
+  //<button>をクリックしたらstopSongsの関数を実行して、宣言した"sounds"のIdを持つメディアの再生を開始する(元々html上で付与していた<audio>のidを、js上の初めに宣言した文字列を用いて指定した)
   btn.addEventListener('click', () => {
     stopSongs()
-
-    //宣言した"sounds"のIdのメディアの再生を開始
     document.getElementById(sound).play()
-
   })
 
-  //元々htmlで記載してある#buttonsの子要素に上記の状態の"btn"を追加
+  //元々htmlで記載してある#buttonsの子要素に上記の状態の"btn"を追加(どこに生成するかの指定)
   document.getElementById('buttons').appendChild(btn)
 })
 
+//音を停止する関数
 function stopSongs() {
   sounds.forEach(sound => {
     //#soundの付与されているエレメントオブジェクトをsongへ代入
-    const song = document.getElementById(sound)
-    
+    const song = document.getElementById(sound)    
     //メディアの再生を一時停止する
     song.pause()
     //現在の再生時間を0にする
