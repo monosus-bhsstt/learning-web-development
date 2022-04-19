6c18cb74bbf9b874390a59ded2622fe70a30727c
mono-yano <yano@monosus.co.jp>
2022-04-19 14:58:02 +0900

    add:script.js(読み取り2)

--- 11_event-keycodes/script.js
+++ 11_event-keycodes/script.js
@@ -5,6 +5,7 @@ const insert = document.getElementById('insert')
 //1 event.key:押されたキーがもしもスペースキーだったらkeyは" "となる。keyが" "だったらdivの中に"Space"と表示、そうでなければ押されたキーのキー名(だいたいキーボードに表示されているもの)を表示する。
 //2 event.keyCode:押されたキーのキーコード名(ほぼ数字)を表示する。
 //3 event.code:押されたキーのコード名(キーの英字表記)を表示する。
+//<small>:免責事項、警告、法的制限、著作権などの注釈や小さく表示されるべき文。今回はどれにも当てはまらない？
 window.addEventListener('keydown', (event) => {
   insert.innerHTML = `
   <div class="key">
