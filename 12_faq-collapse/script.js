2057127b82c0d0ac724d46f5951609ca3e28d1e6
mono-yano <yano@monosus.co.jp>
2022-04-21 17:08:27 +0900

    issues#19:script.js(details・summaryの設定により不要となったのでコメントアウト)

--- 12_faq-collapse/script.js
+++ 12_faq-collapse/script.js
@@ -1,8 +1,10 @@
-const toggles = document.querySelectorAll('.faq-toggle')
+// const toggles = document.querySelectorAll('.faq-toggle')
 
-//"faq-toggle"のクラス名のあるタグをクリックした時。その親要素にクラス名"active"があれば削除して、なければ追加する。
-toggles.forEach(toggle => {
-  toggle.addEventListener('click', () => {
-    toggle.parentNode.classList.toggle('active')
-  })
-})
+// //"faq-toggle"のクラス名のあるタグをクリックした時。その親要素にクラス名"active"があれば削除して、なければ追加する。
+// toggles.forEach(toggle => {
+//   toggle.addEventListener('click', () => {
+//     toggle.parentNode.classList.toggle('active')
+//     //toggle.parentNode.open
+//   console.log(toggle.parentNode)
+//   })
+// })
